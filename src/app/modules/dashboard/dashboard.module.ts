73c3d9ab925050060fabc20df8028c4330192bd0
suraj.pawar <suraj.pawar@sugarboxnetworks.com>
2021-11-22 09:10:54 +0530

    Feature: Replicate styles as per UX

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,11 +5,15 @@ import { StageComponent } from './stage/stage.component';
 import { DashboardComponent } from './dashboard.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardRoutingModule } from './dashboard.routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	imports: [
 		FormsModule,
 		CommonModule,
+		MatIconModule,
+		MatButtonModule,
 		DashboardRoutingModule
 	],
 	exports: [DashboardComponent],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [HomeComponent],
 	imports: [
 		CommonModule,
 		RouterModule,
+		MatButtonModule,
+		MatIconModule,
 		HomeRoutingModule
 	]
 })
